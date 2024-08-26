143546a8e526adb6013fd29e1ddd19dba3ccee60
ArduFish123 <80416599+ArduFish123@users.noreply.github.com>
2024-08-26 16:22:17 +0300

    Create ModConfig.java

---
+++ src/main/java/ro/fishmc/ModConfig.java
@@ -0,0 +1,8 @@
+import me.fzzyhmstrs.fzzy_config.annotations.Comment;
+import me.fzzyhmstrs.fzzy_config.annotations.TomlHeaderComment;
+import me.fzzyhmstrs.fzzy_config.api.ConfigApi;
+import me.fzzyhmstrs.fzzy_config.config.Config;
+import me.fzzyhmstrs.fzzy_config.util.EnumTranslatable;
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedDouble;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
